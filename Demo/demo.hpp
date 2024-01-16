290154df0120bb829e2520ca7acf4adde53e540c
zanadoman <domanseven@gmail.com>
2024-01-16 17:12:57 +0100

    .

---
+++ Demo/demo.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "../SlayEngine/SlayEngine.hpp"
---
+++ Demo/main.cpp
@@ -0,0 +1,10 @@
+#include "demo.hpp"
+
+#undef main
+
+using namespace neo;
+
+sint32 main(sint32 argc, char* *argv)
+{
+    return 0;
+}
