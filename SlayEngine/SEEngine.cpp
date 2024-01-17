f19e63382eb4cfdea2ad84c89da5722f58f7a675
zanadoman <domanseven@gmail.com>
2024-01-17 15:54:35 +0100

    .

--- SlayEngine/SEEngine.cpp
+++ SlayEngine/SEEngine.cpp
@@ -76,4 +76,36 @@ namespace slay
         SDL_DestroyRenderer(this->Renderer);
         SDL_DestroyWindow(this->Window);
     }
+
+    bool engine::Update()
+    {
+        SDL_Event event;
+        uint64 i;
+
+        for (i = 0; SDL_PollEvent(&event); i++)
+        {
+            if (event.type == SDL_QUIT)
+            {
+                return false;
+            }
+
+            if (this->EventQueue.Length() == i)
+            {
+                this->EventQueue += {event};
+            }
+            else
+            {
+                this->EventQueue[i] = {event};
+            }
+        }
+        if (i < this->EventQueue.Length())
+        {
+            this->EventQueue.Remove(i, this->EventQueue.Length() - i);
+        }
+
+        this->UpdateInput();
+        this->Mouse.Update();
+
+        return true;
+    }
 }
