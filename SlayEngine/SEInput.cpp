3223d987e0c3975db92908e27792cf73bbda1c14
zanadoman <domanseven@gmail.com>
2024-01-17 14:14:28 +0100

    .

--- SlayEngine/SEInput.cpp
+++ SlayEngine/SEInput.cpp
@@ -4,9 +4,23 @@ namespace slay
 {
     uint8 engine::UpdateInput()
     {
+        uint32 MouseState;
+
         memCopyTo(this->SDL_KeyStates, this->KeyStates, sizeof(uint8) * KEY_COUNT);
 
-
+        MouseState = SDL_GetMouseState(NULL, NULL);
+        if (MouseState & 1)
+        {
+            this->KeyStates[KEY_LMB] = true;
+        }
+        if (MouseState & 2)
+        {
+            this->KeyStates[KEY_MMB] = true;
+        }
+        if (MouseState & 4)
+        {
+            this->KeyStates[KEY_RMB] = true;
+        }
 
         return 0;
     }
