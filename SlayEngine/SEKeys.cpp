edf84e97844b124afa6b9baf562246fbb311c9c8
zanadoman <domanseven@gmail.com>
2024-01-17 16:08:42 +0100

    .

---
+++ SlayEngine/SEKeys.cpp
@@ -0,0 +1,66 @@
+#include "SlayEngine.hpp"
+
+namespace slay
+{
+    bool engine::GetKey(keys Key)
+    {
+        return this->KeyStates[Key];
+    }
+
+    uint8 engine::UpdateKeys()
+    {
+        uint32 MouseState;
+
+        memCopyTo(this->SDL_KeyStates, this->KeyStates, sizeof(uint8) * KEY_COUNT);
+
+        MouseState = SDL_GetMouseState(NULL, NULL);
+        if (MouseState & 1)
+        {
+            this->KeyStates[KEY_LMB] = true;
+        }
+        else
+        {
+            this->KeyStates[KEY_LMB] = false;
+        }
+        if (MouseState & 2)
+        {
+            this->KeyStates[KEY_MMB] = true;
+        }
+        else
+        {
+            this->KeyStates[KEY_MMB] = false;
+        }
+        if (MouseState & 4)
+        {
+            this->KeyStates[KEY_RMB] = true;
+        }
+        else
+        {
+            this->KeyStates[KEY_RMB] = false;
+        }
+
+        for (uint64 i = 0; i < this->EventQueue.Length(); i++)
+        {
+            if (this->EventQueue[i].type == SDL_MOUSEWHEEL)
+            {
+                if (this->EventQueue[i].wheel.y < 0)
+                {
+                    this->KeyStates[KEY_WHEELDOWN] = true;
+                    this->KeyStates[KEY_WHEELUP] = false;
+                }
+                else if (0 < this->EventQueue[i].wheel.y)
+                {
+                    this->KeyStates[KEY_WHEELDOWN] = false;
+                    this->KeyStates[KEY_WHEELUP] = true;
+                }
+                else
+                {
+                    this->KeyStates[KEY_WHEELDOWN] = false;
+                    this->KeyStates[KEY_WHEELUP] = false;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
