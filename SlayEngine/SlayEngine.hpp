611ec3efbb8ba4ddda23e8f5bfb3825f745a2b2a
zanadoman <domanseven@gmail.com>
2024-01-17 14:07:19 +0100

    .

--- SlayEngine/SlayEngine.hpp
+++ SlayEngine/SlayEngine.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include "Includes/NeoTypes++.hpp"
+
 #include "Includes/SDL.h"
 #include "Includes/SDL_image.h"
 #include "Includes/SDL_ttf.h"
 #include "Includes/SDL_mixer.h"
-#include "Includes/NeoTypes++.hpp"
 
 #include "SEKeys.hpp"
 
