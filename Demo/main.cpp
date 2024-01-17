5a4c20ea24160bb61bf4dae905616be6cba48764
zanadoman <domanseven@gmail.com>
2024-01-17 17:18:27 +0100

    .

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -6,11 +6,11 @@ using namespace slay;
 
 sint32 main(sint32 argc, char* *argv)
 {
-    engine Engine("Demo", 800, 600, 10);
+    engine Engine("Demo", 1920, 1080, 165);
 
     while (Engine.Update())
     {
-        
+
     }
 
     return 0;
