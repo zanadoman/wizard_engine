4211096c4e4564897f2441e920b9f6dafd423aea
zanadoman <domanseven@gmail.com>
2024-01-18 23:20:51 +0100

    .

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -8,9 +8,6 @@ sint32 main(sint32 argc, char* *argv)
 {
     engine Engine(NULL, 1920, 1080, 1000 / 165);
 
-    uint64 Player = Engine.Actors.New();
-    Engine.Camera.Bind(Player);
-
     while (Engine.Update())
     {
         
