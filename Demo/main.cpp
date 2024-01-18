91f92e1ac2d490365791b7412cc3a59671df9c8c
zanadoman <domanseven@gmail.com>
2024-01-19 00:14:24 +0100

    .

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -4,22 +4,9 @@
 
 using namespace slay;
 
-typedef enum
-{
-    ACT_PLAYER
-} actors;
-
-class player
-{
-    public:
-        ~player() {printf("destroyed\n");}
-};
-
 sint32 main(sint32 argc, char* *argv)
 {
     engine Engine(NULL, 1920, 1080, 1000 / 165);
-
-    uint64 Player = Engine.Actors.New(ACT_PLAYER, new player);
 
     while (Engine.Update())
     {
