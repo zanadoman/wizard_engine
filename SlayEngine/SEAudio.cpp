db90641482ce859cd5d7b84e7a59b015819d7959
zanadoman <domanseven@gmail.com>
2024-01-18 20:49:46 +0100

    .

--- SlayEngine/SEAudio.cpp
+++ SlayEngine/SEAudio.cpp
@@ -13,7 +13,7 @@ namespace slay
     {
         if (GlobalVolume < 0 || 1 < GlobalVolume)
         {
-            printf("engine.audio.SetGlobalVolume(): Invalid GlobalVolume\nParams: Volume: %lf\n", GlobalVolume);
+            printf("engine.audio.SetGlobalVolume(): GlobalVolume must be in the range [0, 1]\nParams: Volume: %lf\n", GlobalVolume);
             return 1;
         }
 
