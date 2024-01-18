8d427f7e9711861064c771fac2952b08de8f7b03
zanadoman <domanseven@gmail.com>
2024-01-18 10:43:33 +0100

    .

--- SlayEngine/SEAssets.cpp
+++ SlayEngine/SEAssets.cpp
@@ -1,3 +1,6 @@
+#include "Includes/SDL_image.h"
+#include "Includes/SDL_render.h"
+#include "Includes/SDL_surface.h"
 #include "SlayEngine.hpp"
 
 namespace slay
@@ -6,6 +9,26 @@ namespace slay
 
     uint64 engine::assets::LoadTexture(const char* Path)
     {
-        return 0;
+        SDL_Surface* tmp;
+
+        if (Path == NULL)
+        {
+            printf("engine.assets.LoadTexture(): Path must not be NULL\nParams: Path: %p\n", Path);
+            exit(1);
+        }
+
+        if ((tmp = IMG_Load(Path)) == NULL)
+        {
+            printf("engine.assets.LoadTexture(): IMG_Load() failed\nParams: Path: %s\n", Path);
+            exit(1);
+        }
+        if ((*(this->Textures += {SDL_CreateTextureFromSurface(Engine.Window.Renderer, tmp)}))[this->Textures.Length() - 1] == NULL)
+        {
+            printf("engine.assets.LoadTexture(): SDL_CreateTextureFromSurface() failed\nParams: Path: %s\n", Path);
+            exit(1);
+        }
+        SDL_FreeSurface(tmp);
+
+        return this->Textures.Length() - 1;
     }
 }
