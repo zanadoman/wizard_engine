81813b1565d27403351712fbac8e6caa9feec6ed
zanadoman <domanseven@gmail.com>
2024-01-21 18:15:11 +0100

    .

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -13,17 +13,6 @@ sint32 main(sint32 argc, char* *argv)
 {
     engine Engine(NULL, 1920, 1080, 1000 / 165);
 
-    uint64 Sound1 = Engine.Assets.LoadSound("player_fire.wav");
-    uint64 Sound2 = Engine.Assets.LoadSound("player_fire.wav");
-    uint64 Sound3 = Engine.Assets.LoadSound("player_fire.wav");
-
-    printf("removing 1\n");
-    Engine.Assets.UnloadSound(Sound1);
-    printf("removing 2\n");
-    Engine.Assets.UnloadSound(Sound2);
-    printf("removing 3\n");
-    Engine.Assets.UnloadSound(Sound3);
-
     while (Engine.Update())
     {
         
