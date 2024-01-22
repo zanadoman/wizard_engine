0af9d0b106174b5c0cc9bc5cfe2c28552f6220be
zanadoman <domanseven@gmail.com>
2024-01-22 15:29:54 +0100

    .

--- SlayEngine/SEWindow.cpp
+++ SlayEngine/SEWindow.cpp
@@ -2,7 +2,13 @@
 
 namespace slay
 {
-    engine::window::window(engine& Engine) : Engine(Engine), Window(NULL), Renderer(NULL), Width(0), Height(0) {}
+    engine::window::window(engine& Engine) : Engine(Engine)
+    {
+        this->Window = NULL;
+        this->Renderer = NULL;
+        this->Width = 0;
+        this->Height = 0;
+    }
 
     engine::window::~window()
     {
@@ -22,32 +28,29 @@ namespace slay
 
     uint8 engine::window::New(const char* Title, uint16 Width, uint16 Height)
     {
-        SDL_DestroyRenderer(this->Renderer);
-        SDL_DestroyWindow(this->Window);
-
-        this->Width = Width;
-        this->Height = Height;
-
         if ((this->Window = SDL_CreateWindow(Title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Width, Height, SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_RESIZABLE | SDL_WINDOW_INPUT_GRABBED)) == NULL)
         {
-            printf("engine.window.New(): SDL_CreateWindow() failed\nParams: Title: %s, Width: %d, Height: %d\n", Title, Width, Height);
+            printf("slay::engine.window.New(): SDL_CreateWindow() failed\nParams: Title: %s, Width: %d, Height: %d\n", Title, Width, Height);
             exit(1);
         }
         if ((this->Renderer = SDL_CreateRenderer(this->Window, -1, SDL_RENDERER_ACCELERATED)) == NULL)
         {
-            printf("engine.window.New(): SDL_CreateRenderer() failed\nParams: Title: %s, Width: %d, Height: %d\n", Title, Width, Height);
+            printf("slay::engine.window.New(): SDL_CreateRenderer() failed\nParams: Title: %s, Width: %d, Height: %d\n", Title, Width, Height);
             exit(1);
         }
         if (SDL_RenderSetLogicalSize(this->Renderer, Width, Height) != 0)
         {
-            printf("engine.window.New(): SDL_RenderSetLogicalSize() failed\nParams: Title: %s, Width: %d, Height: %d\n", Title, Width, Height);
+            printf("slay::engine.window.New(): SDL_RenderSetLogicalSize() failed\nParams: Title: %s, Width: %d, Height: %d\n", Title, Width, Height);
             exit(1);
         }
         if (SDL_SetRenderDrawBlendMode(this->Renderer, SDL_BLENDMODE_BLEND) != 0)
         {
-            printf("engine.window.New(): SDL_SetRenderDrawBlendMode() failed\nParams: Title: %s, Width: %d, Height: %d\n", Title, Width, Height);
+            printf("slay::engine.window.New(): SDL_SetRenderDrawBlendMode() failed\nParams: Title: %s, Width: %d, Height: %d\n", Title, Width, Height);
             exit(1);
         }
+
+        this->Width = Width;
+        this->Height = Height;
 
         return 0;
     }
