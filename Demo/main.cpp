8919316a34b2b4e34cac253e5b22e05f426091ac
zanadoman <domanseven@gmail.com>
2024-01-22 16:28:34 +0100

    .

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -12,6 +12,7 @@ sint32 main(sint32 argc, char* *argv)
     uint64 PlayerTexture = Engine.Actors[Player].Textures.New();
     uint64 Texture = Engine.Assets.LoadPNG("test.png");
 
+    Engine.Actors[Player].SetLayer(1);
     Engine.Actors[Player].Textures[PlayerTexture].Width = 100;
     Engine.Actors[Player].Textures[PlayerTexture].Height = 100;
     Engine.Actors[Player].Textures[PlayerTexture].SetTextureID(Texture);
