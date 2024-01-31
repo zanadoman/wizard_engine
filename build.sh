18c4f97016a2b76a098cefb063d026c594a7211d
zanadoman <domanseven@gmail.com>
2024-01-31 20:00:35 +0100

    .

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 SOURCES=$(find . -name '*.cpp')
+LANG_VERSION="gnu++23"
 BUILD_NAME="bin"
 BUILD_FOLDER="Build"
 
 LINUX_COMPILER="g++"
 LINUX_WARNINGS="-Werror -Wall -Wextra"
 LINUX_LIBRARIES="-LSlayEngine/Libraries/Linux -lNeoTypes++ -lfreetype -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer"
+LINUX_EXTRA_FLAGS="-Wl,-rpath=."
 
 WINDOWS_COMPILER="x86_64-w64-mingw32-g++"
 WINDOWS_WARNINGS=""
 WINDOWS_LIBRARIES="-LSlayEngine/Libraries/Windows -lNeoTypes++ -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer"
+WINDOWS_EXTRA_FLAGS="-mwindows"
 
 GREEN="\e[92m"
 RED="\e[91m"
@@ -20,7 +23,7 @@ ENDCOLOR="\e[0m"
 
 if [[ -n $1 && $1 == "-w" ]] || [[ -n $1 && $1 == "--windows" ]]
 then
-    if ${WINDOWS_COMPILER} -m64 -std=gnu++23 -O3 -mwindows ${WINDOWS_WARNINGS} -o ${BUILD_FOLDER}/${BUILD_NAME}.exe ${SOURCES} ${WINDOWS_LIBRARIES} -lm
+    if ${WINDOWS_COMPILER} -m64 -std=${LANG_VERSION} -O3 ${WINDOWS_EXTRA_FLAGS} ${WINDOWS_WARNINGS} -o ${BUILD_FOLDER}/${BUILD_NAME}.exe ${SOURCES} ${WINDOWS_LIBRARIES} -lm
     then
         echo -e "${BLUE}Windows ${GREEN}build successful!${ENDCOLOR}"
         cd Build || exit 1
@@ -40,7 +43,7 @@ fi
 
 if [[ -n $1 && $1 == "-l" ]] || [[ -n $1 && $1 == "--linux" ]]
 then
-    if ${LINUX_COMPILER} -m64 -std=gnu++23 -O3 -Wl,-rpath=. ${LINUX_WARNINGS} -o ${BUILD_FOLDER}/${BUILD_NAME}.out ${SOURCES} ${LINUX_LIBRARIES} -lm
+    if ${LINUX_COMPILER} -m64 -std=${LANG_VERSION} -O3 ${LINUX_EXTRA_FLAGS} ${LINUX_WARNINGS} -o ${BUILD_FOLDER}/${BUILD_NAME}.out ${SOURCES} ${LINUX_LIBRARIES} -lm
     then
         echo -e "${YELLOW}Linux ${GREEN}build successful!${ENDCOLOR}"
         cd Build || exit 1
