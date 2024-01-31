d0f8a72b07828fcc705139a0b41e41663d870df0
zanadoman <domanseven@gmail.com>
2024-01-31 21:25:01 +0100

    .

--- Compiled/main.s
+++ Compiled/main.s
@@ -1,9 +1,12 @@
 	.file	"main.cpp"
 	.text
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"%d %d %d %d\n"
 	.section	.text.unlikely,"ax",@progbits
-.LCOLDB0:
+.LCOLDB1:
 	.section	.text.startup,"ax",@progbits
-.LHOTB0:
+.LHOTB1:
 	.p2align 4
 	.globl	main
 	.type	main, @function
@@ -12,46 +15,52 @@ main:
 	.cfi_startproc
 	.cfi_personality 0x9b,DW.ref.__gxx_personality_v0
 	.cfi_lsda 0x1b,.LLSDA2231
-	pushq	%r13
+	pushq	%r15
 	.cfi_def_cfa_offset 16
-	.cfi_offset 13, -16
+	.cfi_offset 15, -16
 	xorl	%esi, %esi
 	movl	$6, %r8d
 	movl	$1080, %ecx
-	pushq	%r12
+	pushq	%r14
 	.cfi_def_cfa_offset 24
-	.cfi_offset 12, -24
+	.cfi_offset 14, -24
 	movl	$1920, %edx
-	pushq	%rbp
+	pushq	%r13
 	.cfi_def_cfa_offset 32
-	.cfi_offset 6, -32
-	pushq	%rbx
+	.cfi_offset 13, -32
+	pushq	%r12
 	.cfi_def_cfa_offset 40
-	.cfi_offset 3, -40
-	subq	$808, %rsp
-	.cfi_def_cfa_offset 848
+	.cfi_offset 12, -40
+	pushq	%rbp
+	.cfi_def_cfa_offset 48
+	.cfi_offset 6, -48
+	pushq	%rbx
+	.cfi_def_cfa_offset 56
+	.cfi_offset 3, -56
+	subq	$824, %rsp
+	.cfi_def_cfa_offset 880
 	movq	%fs:40, %rax
-	movq	%rax, 792(%rsp)
+	movq	%rax, 808(%rsp)
 	xorl	%eax, %eax
-	leaq	96(%rsp), %rbx
-	movq	%rsp, %r12
-	movq	%rbx, %rdi
+	leaq	112(%rsp), %r14
+	movq	%r14, %rdi
 .LEHB0:
 	call	_ZN4slay6engineC1EPKctth@PLT
 .LEHE0:
-	movq	%rbx, %rsi
-	movq	%r12, %rdi
+	leaq	16(%rsp), %rdi
+	movq	%r14, %rsi
+	movq	%rdi, 8(%rsp)
 .LEHB1:
 	call	_ZN10backgroundC1ERN4slay6engineE@PLT
 .LEHE1:
-	leaq	32(%rsp), %rbp
-	movq	%rbx, %rsi
-	movq	%rbp, %rdi
+	leaq	48(%rsp), %r15
+	movq	%r14, %rsi
+	movq	%r15, %rdi
 .LEHB2:
 	call	_ZN6playerC1ERN4slay6engineE@PLT
 .LEHE2:
-	movq	40(%rsp), %rsi
-	leaq	176(%rsp), %rdi
+	movq	56(%rsp), %rsi
+	leaq	192(%rsp), %rdi
 .LEHB3:
 	call	_ZN4slay6engine6camera4BindEy@PLT
 	jmp	.L2
@@ -59,45 +68,68 @@ main:
 	.p2align 3
 .L3:
 	call	_ZN6player6UpdateEv@PLT
-.L2:
+	leaq	744(%rsp), %rbx
 	movq	%rbx, %rdi
+	call	_ZN4slay6engine6timing12GetFrameTimeEv@PLT
+	movq	%rbx, %rdi
+	movl	%eax, %r13d
+	call	_ZN4slay6engine6timing14GetWorkingTimeEv@PLT
+	movq	%rbx, %rdi
+	movl	%eax, %r12d
+	call	_ZN4slay6engine6timing13GetRenderTimeEv@PLT
+	movq	%rbx, %rdi
+	movl	%eax, %ebp
+	call	_ZN4slay6engine6timing11GetGameTimeEv@PLT
+	movl	%eax, %esi
+	movl	%r13d, %r8d
+	movl	%r12d, %ecx
+	movl	%ebp, %edx
+	leaq	.LC0(%rip), %rdi
+	xorl	%eax, %eax
+	call	printf@PLT
+.L2:
+	movq	%r14, %rdi
 	call	_ZN4slay6engine6UpdateEv@PLT
 .LEHE3:
-	movq	%rbp, %rdi
+	movq	%r15, %rdi
 	testb	%al, %al
 	jne	.L3
 	call	_ZN6playerD1Ev@PLT
-	movq	%r12, %rdi
+	movq	8(%rsp), %rdi
 	call	_ZN10backgroundD1Ev@PLT
-	movq	%rbx, %rdi
+	movq	%r14, %rdi
 	call	_ZN4slay6engineD1Ev@PLT
-	movq	792(%rsp), %rax
+	movq	808(%rsp), %rax
 	subq	%fs:40, %rax
 	jne	.L15
-	addq	$808, %rsp
+	addq	$824, %rsp
 	.cfi_remember_state
-	.cfi_def_cfa_offset 40
+	.cfi_def_cfa_offset 56
 	xorl	%eax, %eax
 	popq	%rbx
-	.cfi_def_cfa_offset 32
+	.cfi_def_cfa_offset 48
 	popq	%rbp
-	.cfi_def_cfa_offset 24
+	.cfi_def_cfa_offset 40
 	popq	%r12
-	.cfi_def_cfa_offset 16
+	.cfi_def_cfa_offset 32
 	popq	%r13
+	.cfi_def_cfa_offset 24
+	popq	%r14
+	.cfi_def_cfa_offset 16
+	popq	%r15
 	.cfi_def_cfa_offset 8
 	ret
 .L15:
 	.cfi_restore_state
 	call	__stack_chk_fail@PLT
 .L11:
-	movq	%rax, %r13
+	movq	%rax, %rbx
 	jmp	.L4
 .L10:
-	movq	%rax, %rbp
+	movq	%rax, %rbx
 	jmp	.L5
 .L9:
-	movq	%rax, %rbp
+	movq	%rax, %rbx
 	jmp	.L6
 	.globl	__gxx_personality_v0
 	.section	.gcc_except_table,"a",@progbits
@@ -134,24 +166,25 @@ main:
 main.cold:
 .LFSB2231:
 .L4:
-	.cfi_def_cfa_offset 848
-	.cfi_offset 3, -40
-	.cfi_offset 6, -32
-	.cfi_offset 12, -24
-	.cfi_offset 13, -16
-	movq	%rbp, %rdi
-	movq	%r13, %rbp
+	.cfi_def_cfa_offset 880
+	.cfi_offset 3, -56
+	.cfi_offset 6, -48
+	.cfi_offset 12, -40
+	.cfi_offset 13, -32
+	.cfi_offset 14, -24
+	.cfi_offset 15, -16
+	movq	%r15, %rdi
 	call	_ZN6playerD1Ev@PLT
 .L5:
-	movq	%r12, %rdi
+	movq	8(%rsp), %rdi
 	call	_ZN10backgroundD1Ev@PLT
 .L6:
-	movq	%rbx, %rdi
+	movq	%r14, %rdi
 	call	_ZN4slay6engineD1Ev@PLT
-	movq	792(%rsp), %rax
+	movq	808(%rsp), %rax
 	subq	%fs:40, %rax
 	jne	.L16
-	movq	%rbp, %rdi
+	movq	%rbx, %rdi
 .LEHB4:
 	call	_Unwind_Resume@PLT
 .LEHE4:
@@ -166,7 +199,7 @@ main.cold:
 	.byte	0x1
 	.uleb128 .LLSDACSEC2231-.LLSDACSBC2231
 .LLSDACSBC2231:
-	.uleb128 .LEHB4-.LCOLDB0
+	.uleb128 .LEHB4-.LCOLDB1
 	.uleb128 .LEHE4-.LEHB4
 	.uleb128 0
 	.uleb128 0
@@ -176,9 +209,9 @@ main.cold:
 	.size	main, .-main
 	.section	.text.unlikely
 	.size	main.cold, .-main.cold
-.LCOLDE0:
+.LCOLDE1:
 	.section	.text.startup
-.LHOTE0:
+.LHOTE1:
 	.hidden	DW.ref.__gxx_personality_v0
 	.weak	DW.ref.__gxx_personality_v0
 	.section	.data.rel.local.DW.ref.__gxx_personality_v0,"awG",@progbits,DW.ref.__gxx_personality_v0,comdat
--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ LINUX_BUILD_FOLDER="Build"
 LINUX_RUN_PREFIX=""
 LINUX_RUN_SUFFIX=""
 
-WINDOWS_ASSEBLY_FOLDER=""
+WINDOWS_ASSEMBLY_FOLDER=""
 WINDOWS_COMPILER="x86_64-w64-mingw32-g++"
 WINDOWS_WARNINGS=""
 WINDOWS_LIBRARIES="-LSlayEngine/Libraries/Windows -lNeoTypes++ -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer"
@@ -49,7 +49,9 @@ then
     if ${LINUX_COMPILER} -m64 -std=${LANGUAGE_VERSION} -O3 ${LINUX_EXTRA_FLAGS} ${LINUX_WARNINGS} -S ${SOURCES}
     then
         echo -e "${YELLOW}Linux ${GREEN}compilation successful!${ENDCOLOR}"
-        rm ${LINUX_ASSEMBLY_FOLDER}/*.s &> /dev/null & mv *.s ${LINUX_ASSEMBLY_FOLDER} &> /dev/null & rm *.s &> /dev/null
+        rm ${LINUX_ASSEMBLY_FOLDER}/*.s &> /dev/null
+        mv *.s ${LINUX_ASSEMBLY_FOLDER} &> /dev/null
+        rm *.s &> /dev/null
     else
         echo -e "${YELLOW}Linux ${RED}compilation failed!${ENDCOLOR}"
         exit 1
@@ -69,6 +71,73 @@ then
         exit 0
     else
         echo -e "${YELLOW}Linux ${RED}run failed!${ENDCOLOR}"
+        exit 1
+    fi
+fi
+
+if [[ -n $1 && $1 == "-w" ]] || [[ -n $1 && $1 == "--windows" ]]
+then
+    ls ${WINDOWS_ASSEMBLY_FOLDER} &> /dev/null || mkdir ${WINDOWS_ASSEMBLY_FOLDER}
+    ls ${WINDOWS_BUILD_FOLDER} &> /dev/null || mkdir ${WINDOWS_BUILD_FOLDER}
+
+    if ${WINDOWS_COMPILER} -m64 -std=${LANGUAGE_VERSION} -O3 ${WINDOWS_EXTRA_FLAGS} ${WINDOWS_WARNINGS} -S ${SOURCES}
+    then
+        echo -e "${BLUE}Windows ${GREEN}compilation successful!${ENDCOLOR}"
+        rm ${WINDOWS_ASSEMBLY_FOLDER}/*.s &> /dev/null
+        mv *.s ${WINDOWS_ASSEMBLY_FOLDER} &> /dev/null
+        rm *.s &> /dev/null
+    else
+        echo -e "${BLUE}Windows ${RED}compilation failed!${ENDCOLOR}"
+        exit 1
+    fi
+
+    if ${WINDOWS_COMPILER} -o ${WINDOWS_BUILD_FOLDER}/${WINDOWS_BUILD_NAME} ${WINDOWS_ASSEMBLY_FOLDER}/*.s ${WINDOWS_LIBRARIES} -lm
+    then
+        echo -e "${BLUE}Windows ${GREEN}linking successful!${ENDCOLOR}"
+    else
+        echo -e "${BLUE}Windows ${RED}linking failed!${ENDCOLOR}"
+        exit 1
+    fi
+
+    if cd ${WINDOWS_BUILD_FOLDER} && ${WINDOWS_RUN_PREFIX} ./${WINDOWS_BUILD_NAME} ${WINDOWS_RUN_SUFFIX}
+    then
+        echo -e "${BLUE}Windows ${GREEN}run successful!${ENDCOLOR}"
+        exit 0
+    else
+        echo -e "${BLUE}Windows ${RED}run failed!${ENDCOLOR}"
+        exit 1
+    fi
+fi
+
+if [[ -n $1 && $1 == "-g" ]] || [[ -n $1 && $1 == "--git" ]]
+then
+    ls ${GIT_ASSEMBLY_FOLDER} &> /dev/null || mkdir ${GIT_ASSEMBLY_FOLDER}
+    ls ${GIT_BUILD_FOLDER} &> /dev/null || mkdir ${GIT_BUILD_FOLDER}
+
+    if ${GIT_COMPILER} -m64 -std=${LANGUAGE_VERSION} -O3 ${GIT_EXTRA_FLAGS} ${GIT_WARNINGS} -S $(find $(git diff --name-only) -name ${GIT_FILTER})
+    then
+        echo -e "${MAGENTA}Git ${GREEN}compilation successful!${ENDCOLOR}"
+        mv *.s ${GIT_ASSEMBLY_FOLDER} &> /dev/null
+        rm *.s &> /dev/null
+    else
+        echo -e "${MAGENTA}Git ${RED}compilation failed!${ENDCOLOR}"
+        exit 1
+    fi
+
+    if ${GIT_COMPILER} -o ${GIT_BUILD_FOLDER}/${GIT_BUILD_NAME} ${GIT_ASSEMBLY_FOLDER}/*.s ${GIT_LIBRARIES} -lm
+    then
+        echo -e "${MAGENTA}Git ${GREEN}linking successful!${ENDCOLOR}"
+    else
+        echo -e "${MAGENTA}Git ${RED}linking failed!${ENDCOLOR}"
+        exit 1
+    fi
+
+    if cd ${GIT_BUILD_FOLDER} && ${GIT_RUN_PREFIX} ./${GIT_BUILD_NAME} ${GIT_RUN_SUFFIX}
+    then
+        echo -e "${MAGENTA}Git ${GREEN}run successful!${ENDCOLOR}"
+        exit 0
+    else
+        echo -e "${MAGENTA}Git ${RED}run failed!${ENDCOLOR}"
         exit 1
     fi
 fi
