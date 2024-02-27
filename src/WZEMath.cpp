306c30d9eb629a01b50ec20a6e802e9baa0a4f2d
zanadoman <domanseven@gmail.com>
2024-02-27 09:33:09 +0100

    .

--- src/WZEMath.cpp
+++ src/WZEMath.cpp
@@ -15,6 +15,17 @@ namespace wze
         return (Value1 - EPSILON <= Value2 && Value2 <= Value1 + EPSILON);
     }
 
+    double engine::math::IfNaN(double Value, double Fallback)
+    {
+        if (Fallback != Fallback)
+        {
+            printf("wze::engine::math::IfNaN(): Fallback must not be NaN\nParams: Value: %lf, Fallback: %lf\n", Value, Fallback);
+            exit(1);
+        }
+
+        return (Value == Value) ? Value : Fallback;
+    }
+
     sint32 engine::math::Random(sint32 Min, sint32 Max)
     {
         if (Max <= Min)
