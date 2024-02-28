93750aadfd376d5c13d67182f91e0cb0fe589841
zanadoman <domanseven@gmail.com>
2024-02-28 11:59:40 +0100

    SHARED

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-g++ -m64 -std=gnu++23 -O3 -c $(find src -name "*.cpp")
-ar rcs "build/lib/linux/libWizardEngine.a" ./*.o
+g++ -m64 -std=gnu++23 -O3 -c -fPIC $(find src -name "*.cpp")
+g++ -shared -o "build/lib/linux/libWizardEngine.so" ./*.o -Lsrc/lib/linux -lNeoTypes++ -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -lSDL2_net
 rm ./*.o
 
 x86_64-w64-mingw32-g++ -m64 -std=gnu++23 -O3 -c $(find src -name "*.cpp")
-ar rcs "build/lib/windows/libWizardEngine.a" ./*.o
+x86_64-w64-mingw32-g++ -shared -o "build/lib/windows/libWizardEngine.dll" ./*.o -Lsrc/lib/windows -lNeoTypes++ -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -lSDL2_net
 rm ./*.o
 
 cp src/*.hpp build/inc
