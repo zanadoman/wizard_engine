0119f7407cc7d72a1e28c1820ab8a6b9b0b1f4fa
zanadoman <domanseven@gmail.com>
2024-05-08 21:12:46 +0200

    .

--- inc/common.hpp
+++ inc/common.hpp
@@ -1,19 +1,19 @@
 #pragma once
 
-#include <cstdint> // IWYU pragma: keep
-#include <string> // IWYU pragma: keep
-#include <vector> // IWYU pragma: keep
-#include <cstddef> // IWYU pragma: keep 
-#include <algorithm> // IWYU pragma: keep
-#include <memory> // IWYU pragma: keep
+#include <cstdint>     // IWYU pragma: keep
+#include <string>      // IWYU pragma: keep
+#include <vector>      // IWYU pragma: keep
+#include <cstddef>     // IWYU pragma: keep 
+#include <algorithm>   // IWYU pragma: keep
+#include <memory>      // IWYU pragma: keep
 #include <type_traits> // IWYU pragma: keep
-#include <stdexcept> // IWYU pragma: keep
+#include <stdexcept>   // IWYU pragma: keep
 
-#include <SDL2/SDL.h> // IWYU pragma: keep
+#include <SDL2/SDL.h>       // IWYU pragma: keep
 #include <SDL2/SDL_image.h> // IWYU pragma: keep
 #include <SDL2/SDL_mixer.h> // IWYU pragma: keep
-#include <SDL2/SDL_ttf.h> // IWYU pragma: keep
-#include <SDL2/SDL_net.h> // IWYU pragma: keep
+#include <SDL2/SDL_ttf.h>   // IWYU pragma: keep
+#include <SDL2/SDL_net.h>   // IWYU pragma: keep
 
 #define PI 3.1415927f
 #define DEG_MAX 360.0f
