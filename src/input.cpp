73fb8cbd67ca93d6602e447701e81baa9078dafc
zanadoman <domanseven@gmail.com>
2024-05-12 14:02:28 +0200

    .

--- src/input.cpp
+++ src/input.cpp
@@ -1,63 +1,83 @@
 #include "../include/WZE/input.hpp" // IWYU pragma: keep
 
-std::array<uint8_t, wze::KEY_COUNT> wze::input::_keys;
-int32_t                             wze::input::_mouse_x_abs = 0;
-int32_t                             wze::input::_mouse_y_abs = 0;
-float                               wze::input::_mouse_x_rel = 0;
-float                               wze::input::_mouse_y_rel = 0;
-float                               wze::input::_mouse_sens  = 1;
+// NOLINTBEGIN
+std::array<int8_t, wze::KEY_COUNT> wze::input::_keys;
+float                              wze::input::_mouse_x    = 0;
+float                              wze::input::_mouse_y    = 0;
+float                              wze::input::_mouse_sens = 1;
+// NOLINTEND
 
 auto wze::input::key(keys key) -> int8_t {
-    return input::_keys[key];
+    return _keys.at(key);
 }
 
-auto wze::input::mouse_x_abs() -> int32_t {
-    return input::_mouse_x_abs;
+auto wze::input::mouse_x() -> float {
+    return _mouse_x;
 }
 
-auto wze::input::mouse_y_abs() -> int32_t {
-    return input::_mouse_y_abs;
-}
-
-auto wze::input::mouse_x_rel() -> float {
-    return input::_mouse_x_rel;
-}
-
-auto wze::input::mouse_y_rel() -> float {
-    return input::_mouse_y_rel;
+auto wze::input::mouse_y() -> float {
+    return _mouse_y;
 }
 
 auto wze::input::mouse_sens() -> float {
-    return input::_mouse_sens;
+    return _mouse_sens;
 }
 
 void wze::input::set_mouse_sens(float sens) {
-    input::_mouse_sens = sens;
+    _mouse_sens = sens;
+}
+
+void wze::input::show_cursor() {
+    SDL_SetRelativeMouseMode(SDL_FALSE);
+}
+
+void wze::input::hide_cursor() {
+    SDL_SetRelativeMouseMode(SDL_TRUE);
+}
+
+auto wze::input::cursor_shown() -> bool {
+    return SDL_GetRelativeMouseMode();
 }
 
 void wze::input::update() {
-    int32_t tmp_x;
-    int32_t tmp_y;
-    uint8_t mouse_state;
+    static const uint16_t               x_max  = window::width() - 1;
+    static const uint16_t               y_max  = window::height() - 1;
+    static const uint8_t               *keys   = SDL_GetKeyboardState(nullptr);
+    static const std::deque<SDL_Event> &events = engine::events();
 
-    (void)memcpy(input::_keys.data(), SDL_GetKeyboardState(nullptr),
-                 KEY_COUNT * sizeof(int8_t));
+    int32_t        x           = 0;
+    int32_t        y           = 0;
+    uint32_t const mouse_state = SDL_GetMouseState(nullptr, nullptr);
 
-    mouse_state = SDL_GetMouseState(nullptr, nullptr);
-    input::_keys[KEY_MOUSE_LMB]   = mouse_state & 1;
-    input::_keys[KEY_MOUSE_MMB]   = mouse_state & 2;
-    input::_keys[KEY_MOUSE_RMB]   = mouse_state & 3;
-    input::_keys[KEY_MOUSE_WHEEL] = 0;
+    memcpy(_keys.data(), keys, KEY_COUNT);
 
-    for (auto event : engine::events()) {
+    _keys[KEY_MOUSE_LMB]   = (int8_t)(mouse_state == 1);
+    _keys[KEY_MOUSE_MMB]   = (int8_t)(mouse_state == 2);
+    _keys[KEY_MOUSE_RMB]   = (int8_t)(mouse_state == 3);
+    _keys[KEY_MOUSE_WHEEL] = 0;
+
+    for (auto event : events) {
+        // NOLINTBEGIN
         if (event.type == SDL_MOUSEWHEEL) {
-            input::_keys[KEY_MOUSE_WHEEL] = event.wheel.y;
+            _keys[KEY_MOUSE_WHEEL] = event.wheel.y;
+            // NOLINTEND
             break;
         }
     }
 
-    (void)SDL_GetMouseState(&input::_mouse_x_abs, &input::_mouse_x_abs);
-    (void)SDL_GetRelativeMouseState(&tmp_x, &tmp_y);
-    input::_mouse_x_rel = tmp_x * input::_mouse_sens;
-    input::_mouse_y_rel = tmp_y * input::_mouse_sens;
+    if (SDL_GetRelativeMouseMode()) {
+        SDL_GetRelativeMouseState(&x, &y);
+        _mouse_x = (float)x * _mouse_sens;
+        _mouse_y = (float)x * _mouse_sens;
+    } else {
+        for (auto event : engine::events()) {
+            // NOLINTBEGIN
+            if (event.type == SDL_MOUSEMOTION) {
+                _mouse_x = (float)std::clamp(event.motion.x, 0, (int32_t)x_max);
+                _mouse_y = (float)std::clamp(event.motion.y, 0, (int32_t)y_max);
+                // NOLINTEND
+                break;
+            }
+        }
+    }
 }
