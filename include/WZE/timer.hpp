2b2c479758651c7f4fe7f1d020b1f5a81e134854
zanadoman <domanseven@gmail.com>
2024-06-11 21:37:47 +0200

    .

--- include/WZE/timer.hpp
+++ include/WZE/timer.hpp
@@ -1,19 +1,21 @@
 /**
- * This file is part of Wizard Engine
- * (https://github.com/zanadoman/Wizard-Engine). Copyright (c) 2024 Zana Domán.
+ * (C) 2023 Zana Domán
  *
- * Wizard Engine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
  *
- * Wizard Engine is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
  *
- * You should have received a copy of the GNU General Public License
- * along with Wizard Engine. If not, see
- * https://www.gnu.org/licenses/licenses.html.
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
  */
 
 #pragma once
@@ -21,70 +23,73 @@
 #include "common.hpp" // IWYU pragma: keep
 
 namespace wze {
-    class timer final {
-        private:
-        static uint8_t _frame_time;
-        static uint8_t _delta_time;
-        static uint64_t _last_time;
+/**
+ * @file timer.hpp
+ * @author Zana Domán
+ * @brief Timing subsystem to handle frame rate and delta time.
+ */
+class timer final {
+  private:
+    static uint8_t _frame_time;
+    static uint8_t _delta_time;
 
-        /**
-         * @file timer.hpp
-         * @author Zana Domán
-         * @brief Private constructor to prevent instantiation.
-         */
-        timer();
+    /**
+     * @file timer.hpp
+     * @author Zana Domán
+     * @brief Private default constructor to prevent instantiation.
+     */
+    timer() = default;
 
-        public:
-        /**
-         * @file timer.hpp
-         * @author Zana Domán
-         * @brief Returns the target frame time in milliseconds.
-         * @return Target frame time in milliseconds.
-         */
-        static uint8_t frame_time();
+  public:
+    /**
+     * @file timer.hpp
+     * @author Zana Domán
+     * @brief Returns the target frame time in milliseconds.
+     * @return Frame time in milliseconds.
+     */
+    static uint8_t frame_time();
 
-        /**
-         * @file timer.hpp
-         * @author Zana Domán
-         * @brief Sets the target frame time to the given value in milliseconds.
-         * @param frame_time Frame time in milliseconds.
-         */
-        static void set_frame_time(uint8_t frame_time);
+    /**
+     * @file timer.hpp
+     * @author Zana Domán
+     * @brief Sets the target frame time to the given value in milliseconds.
+     * @param frame_time Frame time in milliseconds.
+     */
+    static void set_frame_time(uint8_t frame_time);
 
-        /**
-         * @file timer.hpp
-         * @author Zana Domán
-         * @brief Returns the current delta time in milliseconds.
-         * @return Current delta time in milliseconds.
-         */
-        static uint8_t delta_time();
+    /**
+     * @file timer.hpp
+     * @author Zana Domán
+     * @brief Returns the current delta time in milliseconds.
+     * @return Delta time in milliseconds.
+     */
+    static uint8_t delta_time();
 
-        /**
-         * @file timer.hpp
-         * @author Zana Domán
-         * @brief Sets the current delta time to the given value in
-         * milliseconds.
-         * @param delta_time Delta time in milliseconds.
-         * @note You can change the pace of the game by manipulating this value.
-         */
-        static void set_delta_time(uint8_t delta_time);
+    /**
+     * @file timer.hpp
+     * @author Zana Domán
+     * @brief Sets the current delta time to the given value in milliseconds.
+     * @param delta_time Delta time in milliseconds.
+     * @note You can change the pace of the game by manipulating this value.
+     */
+    static void set_delta_time(uint8_t delta_time);
 
-        /**
-         * @file timer.hpp
-         * @author Zana Domán
-         * @brief Returns the current game time in milliseconds.
-         * @return Current game time in milliseconds.
-         */
-        static uint64_t current_time();
+    /**
+     * @file timer.hpp
+     * @author Zana Domán
+     * @brief Delays the current frame to match the target frame time if needed,
+     * then sets the delta time for the next frame.
+     * @warning This method is handled by the engine itself, calling it
+     * explicitly can lead to undefined behavior.
+     */
+    static void __update();
 
-        /**
-         * @file timer.hpp
-         * @author Zana Domán
-         * @brief Delays the current frame to match the frame time if needed,
-         * then sets the delta time for the next frame.
-         * @warning This method is handled by the engine itself, calling it
-         * explicitly can lead to undefined behavior.
-         */
-        static void __update();
-    };
+    /**
+     * @file timer.hpp
+     * @author Zana Domán
+     * @brief Returns the current game time in milliseconds.
+     * @return Game time in milliseconds.
+     */
+    static uint64_t current_time();
+};
 } // namespace wze
--- src/timer.cpp
+++ src/timer.cpp
@@ -1,26 +1,27 @@
 /**
- * This file is part of Wizard Engine
- * (https://github.com/zanadoman/Wizard-Engine). Copyright (c) 2024 Zana Domán.
+ * (C) 2023 Zana Domán
  *
- * Wizard Engine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
  *
- * Wizard Engine is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
  *
- * You should have received a copy of the GNU General Public License
- * along with Wizard Engine. If not, see
- * https://www.gnu.org/licenses/licenses.html.
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
  */
 
 #include "WZE/timer.hpp"
 
 uint8_t wze::timer::_frame_time = 0;
 uint8_t wze::timer::_delta_time = 0;
-uint64_t wze::timer::_last_time = 0;
 
 uint8_t wze::timer::frame_time() {
     return _frame_time;
@@ -38,22 +39,23 @@ void wze::timer::set_delta_time(uint8_t delta_time) {
     _delta_time = delta_time;
 }
 
-uint64_t wze::timer::current_time() {
-    return SDL_GetTicks64();
-}
-
 void wze::timer::__update() {
+    static uint64_t last = 0;
     uint64_t now;
     uint64_t end;
 
     now = current_time();
-    end = _last_time + _frame_time;
+    end = last + frame_time();
 
     if (now < end) {
         SDL_Delay(end - now);
     }
 
     now = current_time();
-    _delta_time = now - _last_time;
-    _last_time = now;
+    set_delta_time(now - last);
+    last = now;
+}
+
+uint64_t wze::timer::current_time() {
+    return SDL_GetTicks64();
 }
