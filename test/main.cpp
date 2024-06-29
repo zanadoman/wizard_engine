20c094991d92ef47d38abaf3492d01547ab6e25e
zanadoman <domanseven@gmail.com>
2024-06-29 09:28:04 +0200

    .

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,3 @@
-#include "wizard_engine/render.hpp"
 #include <wizard_engine/wizard_engine.hpp>
 
 wze_main(1920, 1080) {
@@ -7,39 +6,39 @@ wze_main(1920, 1080) {
     std::unique_ptr<wze::entity> entity;
 
     wizard =
-        wze::sprite::create(0.f, 0.f, 0.f, 0.f, 100.f, 100.f, false,
+        wze::sprite::create(0, 0, 500, 0, 100, 100, true,
                             wze::assets::create_texture(wze::assets::load_image(
                                 "./assets/wizard_engine/icon.png")));
     cat =
-        wze::sprite::create(0.f, 0.f, 0.f, 0.f, 50.f, 50.f, false,
+        wze::sprite::create(0, 0, 400, 0, 50, 50, true,
                             wze::assets::create_texture(wze::assets::load_image(
                                 "./assets/test/run1.png")));
 
     wze::renderer::queue().push_back(wizard);
     wze::renderer::queue().push_back(cat);
 
-    entity = wze::entity::create(0.f, 0.f, 0.f, {wizard, cat});
+    entity = wze::entity::create(0, 0, 0, {wizard, cat});
 
     wze_while(true) {
         if (wze::input::key(wze::KEY_UP)) {
-            entity->set_x(entity->x() +
-                          wze::math::move_x(0.5f * wze::timer::delta_time(),
-                                            entity->angle() -
-                                                wze::math::to_radians(90.f)));
-            entity->set_y(entity->y() +
-                          wze::math::move_y(0.5f * wze::timer::delta_time(),
-                                            entity->angle() -
-                                                wze::math::to_radians(90.f)));
+            entity->set_x(
+                entity->x() +
+                wze::math::move_x(0.5 * wze::timer::delta_time(),
+                                  entity->angle() - wze::math::to_radians(90)));
+            entity->set_y(
+                entity->y() +
+                wze::math::move_y(0.5 * wze::timer::delta_time(),
+                                  entity->angle() - wze::math::to_radians(90)));
         }
         if (wze::input::key(wze::KEY_DOWN)) {
-            entity->set_x(entity->x() +
-                          wze::math::move_x(-0.5f * wze::timer::delta_time(),
-                                            entity->angle() -
-                                                wze::math::to_radians(90.f)));
-            entity->set_y(entity->y() +
-                          wze::math::move_y(-0.5f * wze::timer::delta_time(),
-                                            entity->angle() -
-                                                wze::math::to_radians(90.f)));
+            entity->set_x(
+                entity->x() +
+                wze::math::move_x(-0.5 * wze::timer::delta_time(),
+                                  entity->angle() - wze::math::to_radians(90)));
+            entity->set_y(
+                entity->y() +
+                wze::math::move_y(-0.5 * wze::timer::delta_time(),
+                                  entity->angle() - wze::math::to_radians(90)));
         }
         if (wze::input::key(wze::KEY_LEFT)) {
             entity->set_angle(entity->angle() -
