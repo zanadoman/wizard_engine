0455195d719d9fd824fa6f89206e99527216c378
zanadoman <domanseven@gmail.com>
2024-08-02 15:32:04 +0200

    .

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,9 +6,9 @@ wze_main(200, 300) {
         wze::assets::load_image("./assets/wizard_engine/logo.png"));
     wze::sprite sprite(0, 0, 0, 0, 100, 100, false, tex);
 
-    emscripten_set_main_loop([]() {
-        (void)wze::engine::update();
-    }, 0, 1);
+    // emscripten_set_main_loop([]() {
+    //     (void)wze::engine::update();
+    // }, 0, 1);
 
     return 0;
 }
