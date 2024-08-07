e6e12a988fab133bdcdc79ae9a5bf53da117ca9b
zanadoman <domanseven@gmail.com>
2024-08-07 17:43:19 +0200

    .

--- src/server/main.rs
+++ src/server/main.rs
@@ -20,73 +20,80 @@
 */
 
 use anyhow::{anyhow, Error};
-use std::net::SocketAddr;
+use std::{env::args, net::SocketAddr};
 use tokio::{
-    io::{split, AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf},
+    io::{split, AsyncReadExt, AsyncWriteExt},
     main,
-    net::{TcpListener, TcpStream},
+    net::TcpListener,
     spawn,
-    sync::{
-        broadcast,
-        broadcast::{Receiver, Sender},
-    },
+    sync::broadcast,
     try_join,
 };
 
-async fn reader(
-    mut reader: ReadHalf<TcpStream>,
-    address: SocketAddr,
-    transmitter: Sender<(SocketAddr, Vec<u8>)>,
-) -> Result<(), Error> {
-    let mut buffer = [0; 1024];
-
-    loop {
-        let message = match reader.read(&mut buffer).await {
-            Ok(0) => return Err(anyhow!("Disconnected: {}", address)),
-            Ok(size) => (address, buffer[..size].to_vec()),
-            Err(error) => return Err(anyhow!(error)),
-        };
-        print!("{}: {}", message.0, String::from_utf8_lossy(&message.1));
-        if let Err(error) = transmitter.send(message) {
-            return Err(anyhow!(error));
-        }
-    }
-}
-
-async fn writer(
-    mut writer: WriteHalf<TcpStream>,
-    address: SocketAddr,
-    mut receiver: Receiver<(SocketAddr, Vec<u8>)>,
-) -> Result<(), Error> {
-    loop {
-        match receiver.recv().await {
-            Ok(message) => {
-                if message.0 != address {
-                    if let Err(error) = writer.write_all(&message.1).await {
-                        return Err(anyhow!(error));
-                    }
-                }
-            }
-            Err(error) => return Err(anyhow!(error)),
-        }
-    }
-}
-
 #[main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let listener = TcpListener::bind(format!(
+        "0.0.0.0:{}",
+        Vec::<String>::from_iter(args())
+            .get(1)
+            .unwrap_or(&8080.to_string())
+    ))
+    .await
+    .unwrap();
     let (transmitter, ..) = broadcast::channel::<(SocketAddr, Vec<u8>)>(100);
 
     loop {
         let (socket, address) = listener.accept().await.unwrap();
-        let (readr, writr) = split(socket);
+        let (mut reader, mut writer) = split(socket);
         let transmitter = transmitter.clone();
 
         spawn(async move {
             println!("Connected: {}", address);
             if let Err(error) = try_join!(
-                writer(writr, address, transmitter.subscribe()),
-                reader(readr, address, transmitter)
+                async {
+                    let mut buffer = [0; 1024];
+
+                    loop {
+                        let message = match reader.read(&mut buffer).await {
+                            Ok(0) => {
+                                return Err::<(), Error>(anyhow!(
+                                    "Disconnected: {}",
+                                    address
+                                ))
+                            }
+                            Ok(size) => (address, buffer[..size].to_vec()),
+                            Err(error) => return Err(anyhow!(error)),
+                        };
+                        println!(
+                            "{}: {}",
+                            message.0,
+                            String::from_utf8_lossy(&message.1)
+                        );
+                        if let Err(error) = transmitter.send(message) {
+                            return Err(anyhow!(error));
+                        }
+                    }
+                },
+                async {
+                    let mut receiver = transmitter.subscribe();
+
+                    loop {
+                        match receiver.recv().await {
+                            Ok(message) => {
+                                if message.0 != address {
+                                    if let Err(error) =
+                                        writer.write_all(&message.1).await
+                                    {
+                                        return Err::<(), Error>(anyhow!(
+                                            error
+                                        ));
+                                    }
+                                }
+                            }
+                            Err(error) => return Err(anyhow!(error)),
+                        }
+                    }
+                }
             ) {
                 eprintln!("{}", error);
             }
