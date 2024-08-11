cfb00692fa31d249b17917bd8b4cdd6ef2392d99
zanadoman <domanseven@gmail.com>
2024-08-11 13:59:32 +0200

    .

---
+++ android/app/src/main/java/com/example/wizard_engine/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.wizard_engine;
+
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity {
+    @Override
+    protected String[] getLibraries() {
+        return new String[] {
+                "SDL2",
+                "SDL2_image",
+                "SDL2_mixer",
+                "SDL2_ttf",
+                "SDL2_net",
+                "wizard_engine",
+                "main"
+        };
+    }
+}
