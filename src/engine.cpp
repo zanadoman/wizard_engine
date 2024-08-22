cbc36d0150f202a54f2fe8bf55e8f6c82084fc22
zanadoman <domanseven@gmail.com>
2024-08-22 18:30:55 +0200

    .

--- src/engine.cpp
+++ src/engine.cpp
@@ -41,7 +41,7 @@ void wze::engine::play_intro() {
     static_assert(sizeof(size_t) == 4 || sizeof(size_t) == 8);
     constexpr size_t logo_hash =
         sizeof(size_t) == 4 ? 291788640 : 14466144734576808089U;
-    constexpr float scale = .75;
+    constexpr float scale = .6;
     constexpr float speed = .1;
 
     std::shared_ptr<image> logo;
