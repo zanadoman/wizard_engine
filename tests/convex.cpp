9e37dd701661df0520f3389fa8e036ddd79f2ec5
zanadoman <domanseven@gmail.com>
2024-09-10 18:44:53 +0200

    .

--- tests/convex.cpp
+++ tests/convex.cpp
@@ -19,89 +19,98 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-#include <iostream>
 #include <wizard_engine/wizard_engine.hpp>
 
-int64_t cross_product(std::pair<int64_t, int64_t> const& origo,
-                      std::pair<int64_t, int64_t> const& point1,
-                      std::pair<int64_t, int64_t> const& point2) {
-    return (point1.first - origo.first) * (point2.second - origo.second) -
-           (point1.second - origo.second) * (point2.first - origo.first);
-}
+std::vector<std::pair<float, float>>
+extract_image(std::shared_ptr<wze::image> const& image) {
+    std::vector<std::pair<float, float>> extracted;
+    int32_t half_width;
+    int32_t half_height;
+    int32_t i;
+    int32_t j;
 
-std::vector<std::pair<int64_t, int64_t>>
-create_convex(int32_t const* pixels, size_t width, size_t height) {
-    size_t half_width;
-    size_t half_height;
-    size_t i;
-    size_t j;
-    std::vector<std::pair<int64_t, int64_t>> points;
-    // std::vector<std::pair<int64_t, int64_t>> convex;
-    // size_t k;
+    if (image->format->BytesPerPixel != 4) {
+        return extracted;
+    }
 
-    half_width = width / 2;
-    half_height = height / 2;
-    for (i = 0; i != width; ++i) {
-        for (j = 0; j != height; ++j) {
-            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-            if ((bool)pixels[i + j * height]) {
-                points.emplace_back(i - half_width, j - half_height);
+    half_width = image->w / 2;
+    half_height = image->h / 2;
+    for (i = 0; i != image->w; ++i) {
+        for (j = 0; j != image->h; ++j) {
+            // NOLINTNEXTLINE(bugprone-implicit-widening-of-multiplication-result,cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            if ((bool)static_cast<uint8_t*>(
+                    image->pixels)[(i + j * image->h) * 4]) {
+                extracted.emplace_back(i - half_width, j - half_height);
             }
         }
     }
 
-    // if (points.size() <= 3) {
-    //     return points;
-    // }
-    //
-    // std::sort(points.begin(), points.end(),
-    //           [](std::pair<float, float> const& point1,
-    //              std::pair<float, float> const& point2) {
-    //               return point1.first < point2.first ||
-    //                      (point1.first == point2.first &&
-    //                       point1.second < point2.second);
-    //           });
-    // convex.resize(points.size() * 2);
-    //
-    // for (i = 0, j = 0; i < points.size(); ++i) {
-    //
-    //     while (2 <= j && cross_product(convex.at(j - 2), convex.at(j - 1),
-    //                                    points.at(i)) <= 0) {
-    //         --j;
-    //     }
-    //     convex.at(j++) = points.at(i);
-    // }
-    //
-    // for (i = points.size() - 1, k = j + 1; 0 < i; --i) {
-    //     while (j >= k && cross_product(convex.at(j - 2), convex.at(j - 1),
-    //                                    points.at(i - 1)) <= 0) {
-    //         --j;
-    //     }
-    //     convex.at(j++) = points.at(i - 1);
-    // }
-    //
-    // convex.resize(j - 1);
-    return points;
+    return extracted;
+}
+
+float cross_product(std::pair<float, float> const& origo,
+                    std::pair<float, float> const& point1,
+                    std::pair<float, float> const& point2) {
+    return (point1.first - origo.first) * (point2.second - origo.second) -
+           (point1.second - origo.second) * (point2.first - origo.first);
+}
+
+std::vector<std::pair<float, float>>
+create_convex_hull(std::vector<std::pair<float, float>> const& points) {
+    std::vector<std::pair<float, float>> convex_hull;
+    size_t i;
+    size_t j;
+    size_t k;
+
+    if (points.size() <= 3) {
+        return convex_hull;
+    }
+
+    convex_hull.resize(points.size() * 2);
+    for (i = 0, j = 0; i != points.size(); ++i) {
+        while (2 <= j &&
+               cross_product(convex_hull.at(j - 2), convex_hull.at(j - 1),
+                             points.at(i)) <= 0) {
+            --j;
+        }
+        convex_hull.at(j++) = points.at(i);
+    }
+    for (i = points.size() - 1, k = j + 1; (bool)i; --i) {
+        while (k <= j &&
+               cross_product(convex_hull.at(j - 2), convex_hull.at(j - 1),
+                             points.at(i - 1)) <= 0) {
+            --j;
+        }
+        convex_hull.at(j++) = points.at(i - 1);
+    }
+    convex_hull.resize(j - 1);
+
+    return convex_hull;
 }
 
 wze_main("Wizard Engine - Spatial", 1920, 1080) {
     std::shared_ptr<wze::image> image;
+    std::vector<std::pair<float, float>> extracted_image;
+    std::vector<std::pair<float, float>> convex_hull;
     std::shared_ptr<wze::texture> texture;
-    std::vector<std::pair<int64_t, int64_t>> convex;
-    std::vector<wze::sprite> pixels;
+    std::vector<wze::sprite> sprites;
 
     image = wze::assets::load_image("./assets/wizard_engine/icon.png");
+    extracted_image = extract_image(image);
+    convex_hull = create_convex_hull(extracted_image);
+
     texture = wze::assets::create_texture(
         wze::assets::load_image("./assets/tests/image.png"));
-    convex =
-        create_convex(static_cast<int32_t*>(image->pixels), image->w, image->h);
-
-    std::for_each(convex.begin(), convex.end(),
-                  [&](std::pair<int64_t, int64_t> const& vertex) {
-                      pixels.emplace_back(vertex.first * 4, vertex.second * 4,
-                                          0, 0, 4, 4, false, texture);
-                      std::cout << vertex.first << ", " << vertex.second
-                                << '\n';
+    std::for_each(extracted_image.begin(), extracted_image.end(),
+                  [&](std::pair<float, float> const& pixel) {
+                      sprites.emplace_back(pixel.first * 4, pixel.second * 4, 0,
+                                           0, 4, 4, false, texture);
+                  });
+    std::for_each(convex_hull.begin(), convex_hull.end(),
+                  [&](std::pair<float, float> const& pixel) {
+                      sprites.emplace_back(
+                          pixel.first * 4, pixel.second * 4, 0, 0, 4, 4, false,
+                          texture, std::numeric_limits<uint8_t>::max(), 0, 0);
                   });
 
     wze_while(true);
