b135daa75757d18cc1a709d96759f5bfe41ae7ec
zanadoman <domanseven@gmail.com>
2024-09-14 16:05:50 +0200

    .

---
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.app;
+
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity {
+    @override
+    protected String[] getLibraries() {
+        return new String[] {
+                "SDL2",
+                "SDL2_image",
+                "SDL2_mixer",
+                "SDL2_ttf",
+                "SDL2_net",
+                "main"
+        };
+    }
+}
